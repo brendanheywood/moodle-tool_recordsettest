38714f52195b487f2e38d5ebc84cb9eec7492972
Brendan Heywood <brendan@catalyst-au.net>
2017-10-04 17:06:53 +1100

    init

---
+++ recordset_tester.php
@@ -0,0 +1,94 @@
+<?php
+
+define('CLI_SCRIPT', true);
+require_once('config.php');
+
+
+$size = 10000; #start sise
+$ramp = 1.5;  # how quickly is ramps up
+$delay = 1; # sleep for 1 second between runs
+$limit = 1024 * 1024 * 2; # db rows maximum
+
+$use_rs = true;  # use get_recordset
+#$use_rs = false; # use get_records
+
+function mem() {
+    // return memory_get_usage(false); # this is base php memory but excludes pg client memory
+    // return memory_get_usage(true); # this is base php memory but should include other stuff??
+    // return memory_get_peak_usage(false); # this is peak php memory
+    return memory_get_peak_usage(true); # this is peak php memory
+}
+
+function convert($size) {
+    if ($size == 0) {
+        return '0';
+    }
+
+    $unit=array('b','kb','mb','gb','tb','pb');
+    return @round($size/pow(1024,($i=floor(log($size,1024)))),0) . $unit[$i];
+}
+
+$memory_base = mem();
+
+print "top -p ".getmypid() ." # cut and paste this\n";
+sleep(3); # so you can cut and paste the top command
+
+printf("Start memory = %s \n", convert($memory_base));
+
+echo "Size             RS      Loop     After Count\n";
+
+for(;;) {
+
+    $sql = "
+SELECT
+    numbers AS id,
+    concat('content', numbers) AS content
+FROM
+    generate_series(1, $size) AS numbers
+;
+";
+
+    $mem_before_rs = mem();
+
+    if ($use_rs) {
+        $rs = $DB->get_recordset_sql($sql);
+    } else {
+        $rs = $DB->get_records_sql($sql);
+    }
+
+    $mem_before_loop = mem();
+
+    $count = 0;
+
+    foreach ($rs as $record) {
+        $count += $record->id;
+    }
+
+    sleep($delay);
+
+    $mem_after_loop = mem();
+
+
+    printf("%-9d %9s %9s %9s %d\n",
+        $size,
+        convert($mem_before_rs   - $memory_base),
+        convert($mem_before_loop - $memory_base),
+        convert($mem_after_loop  - $memory_base),
+        $count
+    );
+
+    if ($use_rs) {
+        $rs->close();
+    }
+
+    $size = floor($size * $ramp);
+    if ($size > $limit) {
+        break;
+    }
+}
+
+$memory_base = mem();
+
+printf("End memory = %s \n", convert($memory_base));
+
+
